12eb3c157087b1faeee95e08328016cfaf5febce
catmblake <catmblake@gmail.com>
2018-11-14 19:09:52 -0800

    added js pseudocode, html style and script links

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// Trivia Game
+// create a countdown timer for the game
+// create a trivia form w/ multiple choice questions
+// player can only select one answer per question
+// The game ends when the time runs out. 
+// The page will reveal the number of questions that 
+// players answer correctly and incorrectly.
