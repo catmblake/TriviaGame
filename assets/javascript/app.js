8a83cf5003848d504d014e1b0c9f9a6c34bb00dc
catmblake <catmblake@gmail.com>
2018-11-15 14:26:07 -0800

    created basic html structure, added question array in js and began to declare functions I will need

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,67 @@
 // The game ends when the time runs out. 
 // The page will reveal the number of questions that 
 // players answer correctly and incorrectly.
+$("#game-container").text("Trivia Game")
+
+var gameContainer = $("#game-container");
+var resultsContainer = $("#game-results");
+var submitButton = $("#submit");
+
+function askQuestions () {
+}
+
+function displayResults () {
+    console.log("results")
+}
+
+askQuestions ();
+
+$("#submit").on("click", displayResults);
+// Question array containing question/answers as objects
+var myQuestions = [
+    {
+    question: "What is the distance from the earth to the moon?",
+    answers:{
+        a: "39,300 miles",
+        b: "283,700 miles",
+        c: "238,900 miles"
+        },
+        correctAnswer: "c"
+    },
+    {
+    question: "What is the largest planet in our solar system?",
+    answers:{
+        a: "Jupiter",
+        b: "Mars",
+        c: "Saturn"
+        },
+        correctAnswer: "a"
+    },
+    {
+    question: "What planet is closest to the sun?",
+    answers:{
+        a: "Venus",
+        b: "Mars",
+        c: "Mercury"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who was the first man on the moon?",
+        answers:{
+            a: "Neil Armstrong",
+            b: "Uri Gagarin",
+            c: "Michael Collins"
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "What is the name of the galaxy we live in?",
+            answers:{
+                a: "The Solar System",
+                b: "The Milky Way",
+                c: "Andromeda"
+                },
+                correctAnswer: "b"
+            },
+]
