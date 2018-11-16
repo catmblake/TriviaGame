ae8dc2732b1e72d23dea75468d29dd1939626393
catmblake <catmblake@gmail.com>
2018-11-15 20:38:23 -0800

    Working countdown timer,functions to hide and show elements on click of start button and on timeout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,34 @@
-// Trivia Game
-// create a countdown timer for the game
+$("#game-container").hide();
+$("#start").on("click", start);
+
+function start() {
+    $("#start-container").hide();
+    askQuestions();
+    runTimer();
+}
+
+var timeLeft = 30;
+var intervalId;
+
+function runTimer() {
+    clearInterval(intervalId);
+    intervalId = setInterval(countdown, 1000);
+}
+
+function stop() {
+    clearInterval(intervalId);
+}
+
+function countdown() {
+    timeLeft--;
+
+    $("#countdown-timer").html("<h3>Time Remaining: " + timeLeft + " seconds</h3>");
+
+    if (timeLeft === 0) {
+        displayResults();
+        stop();
+    }
+}
 // create a trivia form w/ multiple choice questions
 // player can only select one answer per question
 // The game ends when the time runs out. 
@@ -10,66 +39,67 @@
 // Question array containing question/answers as objects
 var myQuestions = [
     {
-    question: "What is the distance from the earth to the moon?",
-    answers:{
-        a: "39,300 miles",
-        b: "283,700 miles",
-        c: "238,900 miles"
+        question: "What is the distance from the earth to the moon?",
+        answers: {
+            a: "39,300 miles",
+            b: "283,700 miles",
+            c: "238,900 miles"
         },
         correctAnswer: "c"
     },
     {
-    question: "What is the largest planet in our solar system?",
-    answers:{
-        a: "Jupiter",
-        b: "Mars",
-        c: "Saturn"
+        question: "What is the largest planet in our solar system?",
+        answers: {
+            a: "Jupiter",
+            b: "Mars",
+            c: "Saturn"
         },
         correctAnswer: "a"
     },
     {
-    question: "What planet is closest to the sun?",
-    answers:{
-        a: "Venus",
-        b: "Mars",
-        c: "Mercury"
+        question: "What planet is closest to the sun?",
+        answers: {
+            a: "Venus",
+            b: "Mars",
+            c: "Mercury"
         },
         correctAnswer: "c"
     },
     {
         question: "Who was the first man on the moon?",
-        answers:{
+        answers: {
             a: "Neil Armstrong",
             b: "Uri Gagarin",
             c: "Michael Collins"
-            },
-            correctAnswer: "a"
         },
-        {
-            question: "What is the name of the galaxy we live in?",
-            answers:{
-                a: "The Solar System",
-                b: "The Milky Way",
-                c: "Andromeda"
-                },
-                correctAnswer: "b"
-            },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the name of the galaxy we live in?",
+        answers: {
+            a: "The Solar System",
+            b: "The Milky Way",
+            c: "Andromeda"
+        },
+        correctAnswer: "b"
+    },
 ]
-function askQuestions () {
+function askQuestions() {
+    $("#game-container").show();
     var questionNum = 1;
+
     for (i = 0; i < myQuestions.length; i++) {
         console.log(myQuestions[i].question);
         console.log(myQuestions[i].answers);
-        $("#game-container").append("<label>" + questionNum + ". " + myQuestions[i].question + "</label><br>");
-        $("#game-container").append('<input type="radio"> a. ' + myQuestions[i].answers.a + ' </input><input type="radio"> b. ' + myQuestions[i].answers.b + ' </input><input type="radio"> c. ' + myQuestions[i].answers.c + '</input><br>');
-        questionNum ++
+        $(".form").append("<label>" + questionNum + ". " + myQuestions[i].question + "</label><br>");
+        $(".form").append('<input type="radio"> ' + myQuestions[i].answers.a + ' </input><input type="radio"> ' + myQuestions[i].answers.b + ' </input><input type="radio"> ' + myQuestions[i].answers.c + '</input><br>');
+        questionNum++
     }
 }
 
-function displayResults () {
-    console.log("results")
+function displayResults() {
+    $("#game-container").hide();
+    $("#game-results").show();
 }
-
-askQuestions ();
 
 $("#submit").on("click", displayResults);
